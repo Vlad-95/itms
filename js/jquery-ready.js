a4c7728d970293e6bd132f4b6f37b4669fee2701
Vladislav <vzv@siluetstudio.com>
2021-12-15 15:20:14 +0300

    reviews adaptive

--- js/jquery-ready.js
+++ js/jquery-ready.js
@@ -61,6 +61,34 @@ $(document).ready(function() {
         });
     }
 
+    //слайдер отзывов
+    if($('.reviews .slider').length) {
+        $('.reviews .slider').slick({
+            slidesToShow: 4,
+            arrows: false,
+            responsive: [
+                {
+                    breakpoint: 992,
+                    settings: {
+                        slidesToShow: 3
+                    }
+                },
+                {
+                    breakpoint: 768,
+                    settings: {
+                        slidesToShow: 2
+                    }
+                },
+                {
+                    breakpoint: 576,
+                    settings: {
+                        slidesToShow: 1
+                    }
+                }
+            ]
+        })
+    }
+
     //слайдер партнеров
     if ($('.partners .slider').length) {
         $('.partners .slider').slick({
